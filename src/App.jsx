d8bdcdf6e86b5633b2ece921e5c1238a1d64cc39
Client Link Revert <client-link-revert@noreply.local>
2026-04-09 15:37:16 +0000

    Revert "feat(dashboard): add /dashboard with label from src/assets"
    This reverts commit be166aab1e34b3ab5c7fdc8da3138a26f85a95d6.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header'
 import { LeftSidebar } from './components/LeftSidebar'
 import { RightSidebar } from './components/RightSidebar'
 import { Feed } from './components/Feed'
-import { DashboardPage } from './pages/DashboardPage'
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <div className="fb-app">
-            <Header />
-            <div className="fb-layout">
-              <LeftSidebar />
-              <Feed />
-              <RightSidebar />
-            </div>
-          </div>
-        }
-      />
-      <Route path="/dashboard" element={<DashboardPage />} />
-    </Routes>
+    <div className="fb-app">
+      <Header />
+      <div className="fb-layout">
+        <LeftSidebar />
+        <Feed />
+        <RightSidebar />
+      </div>
+    </div>
   )
 }
 
